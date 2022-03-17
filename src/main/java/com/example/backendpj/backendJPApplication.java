b71a76617f13b71120646e69755a7121cf041cf9
SerguioL <serguio.l.landa@gmail.com>
2022-03-16 22:11:02 -0500

    first commit

---
+++ src/main/java/com/example/backendpj/backendJPApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class backendJPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(backendJPApplication.class, args);
+    }
+
+}
